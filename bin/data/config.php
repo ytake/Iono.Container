9545e20ec28d138bff348ef0aa6d4b6ba0e40c7f
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2014-11-23 02:51:36 +0900

    fixed syntax

--- bin/data/config.php
+++ bin/data/config.php
@@ -6,12 +6,12 @@
  */
 return [
 
-	// annotation file cache directory, and field injection cache  file only)
-	'cache.path' => __CACHE_PATH__,
+    // annotation file cache directory, and field injection cache  file only)
+    'cache.path' => __CACHE_PATH__,
 
     // @Component, @Scope annotation scan target directory
-	'scan.target.path' => __SCAN_TARGET__,
+    'scan.target.path' => __SCAN_TARGET__,
 
     // doctrine/annotation cache driver("file", "apc", "simple"(no cache))
-	'annotation.cache.driver' => 'file',
+    'annotation.cache.driver' => 'file',
 ];
