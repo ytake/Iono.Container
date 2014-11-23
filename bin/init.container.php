c5321ccab19f5e86fa24b04e8c99dd5c3c48e349
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2014-11-23 21:49:44 +0900

    updated

--- bin/init.container.php
+++ bin/init.container.php
@@ -5,7 +5,7 @@
  * @license http://opensource.org/licenses/MIT MIT
  */
 $temporaryDirectory = dirname(__FILE__);
-$defaultResourcePath = realpath(null) . "/resource";
+$defaultResourcePath = dirname(realpath(null)) . "/resource";
 
 echo "\033[32m[create a default directory structure]\033[0m\n";
 if (!@mkdir($defaultResourcePath, 0777, true)) {
@@ -20,7 +20,7 @@ $file = file_get_contents(dirname(__FILE__) . '/data/scanner.php');
 $config = file_get_contents(dirname(__FILE__) . '/data/config.php');
 
 $config = str_replace("__CACHE_PATH__", '"' . $defaultResourcePath . '"', $config);
-$config = str_replace("__SCAN_TARGET__", '"' . realpath(null) . '"', $config);
+$config = str_replace("__SCAN_TARGET__", '"' . dirname(realpath(null)) . '"', $config);
 
 // put configure file
 if(!@file_put_contents($defaultResourcePath . "/config.php", $config)) {
@@ -30,6 +30,6 @@ if(!@file_put_contents($defaultResourcePath . "/config.php", $config)) {
 // replace default configure file path
 $output = str_replace("__CONFIGURE__", '"' . $defaultResourcePath . '/config.php"', $file);
 // put scanner script file
-if(!@file_put_contents(realpath(null) . "/scanner.php", $output)) {
+if(!@file_put_contents(dirname($defaultResourcePath) . "/scanner.php", $output)) {
     echo "\033[31mFailed to make config file:" .realpath(null) ."/scanner.php, Permission denied\033[0m\n";
 }
